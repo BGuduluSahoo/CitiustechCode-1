2a779de01715579782fe1a116201d852da11d7e9
BabluIT <iasbablu1998@gmail.com>
2021-11-06 20:20:40 +0530

    After change

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test{
       public static void main(String[] args){
-        int a = 10;
-        System.out.println(a);
+        int b = 10;
+        System.out.println(b);
 
       }
 
